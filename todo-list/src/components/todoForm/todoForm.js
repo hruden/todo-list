f034b4227a843ea5192b7d76710a023442994368
hruden <119438895+hruden@users.noreply.github.com>
2024-07-26 12:46:17 +0300

    add todoForm

--- todo-list/src/components/todoForm/todoForm.js
+++ todo-list/src/components/todoForm/todoForm.js
@@ -1,8 +1,23 @@
+import React, { useState } from 'react';
 
-function TodoForm() {
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  };
+
   return (
-    <form >
-      <input/>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
     </form>
   );
 }
