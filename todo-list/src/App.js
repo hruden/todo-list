96e9f0088541632d0c41741be7b53b641b21b6ad
hruden <119438895+hruden@users.noreply.github.com>
2024-07-29 12:57:44 +0300

    add style

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,15 +1,15 @@
-
+import React from 'react';
 import './App.css';
 import TodoList from './components/todoList/todoList';
 
 function App() {
   return (
-      <div>
-        <div>
-          <h1>My To-Do List</h1>
-          <TodoList />
-        </div>
+    <div className="app">
+      <div className="container">
+        <h1>My To-Do List</h1>
+        <TodoList />
       </div>
+    </div>
   );
 }
 
