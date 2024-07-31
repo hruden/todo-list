6d3a3f943a783a62df7b63c45501b29e0ba0dcf9
hruden <119438895+hruden@users.noreply.github.com>
2024-07-31 16:45:36 +0300

    add style for todo

--- todo-list/src/components/todo/todo.js
+++ todo-list/src/components/todo/todo.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./todo.css";
+import { SlCheck } from "react-icons/sl";
+import { SlClose } from "react-icons/sl";
 
 function Todo({ todo, index, completeTodo, removeTodo }) {
   return (
-    <div style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }} className="todo">
-      {todo.text}
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className="todo"
+    >
+      <p className="todoText">{todo.text}</p>
       <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>Remove</button>
+        <button onClick={() => completeTodo(index)}>
+          <SlCheck />
+        </button>
+        <button onClick={() => removeTodo(index)}>
+          <SlClose />
+        </button>
       </div>
     </div>
   );
 }
 
 export default Todo;
-
